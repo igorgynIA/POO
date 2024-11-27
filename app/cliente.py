c3308874acc8cea5d13d691c86b6da9949efdcfe
Igor Dias Aguiar <dias2005aguiar@gmail.com>
2024-11-27 18:03:19 -0300

    Testando commit em Igor

--- app/cliente.py
+++ app/cliente.py
@@ -11,7 +11,7 @@ def index():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login_cliente.html")
 
 @app.route("/signin") 
 def signin():
